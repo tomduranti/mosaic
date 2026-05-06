c2739826a8317f8a3678babba3932b3d652e8553
unknown <tommaso.duranti191@gmail.com>
2026-05-06 18:25:27 +0200

    Fix desktop layout, tbc

--- src/components/atoms/MediaCard/MediaCard.jsx
+++ src/components/atoms/MediaCard/MediaCard.jsx
@@ -19,7 +19,7 @@ export default function MediaCard({ isTrending, release_date, poster_path, media
             { isTrending ?
                 (
                     <article className={`${styles.mediacard}  ${styles['mediacard--trending']}`}>
-                        <div className={styles.mediacard__container} style={{ backgroundImage: !poster_path ? `url(${noImageAvailable})` : `${variables.gradient}, url(https://image.tmdb.org/t/p/w500${poster_path})` }}>
+                        <div className={`${styles.mediacard__container}  ${styles['mediacard__container--trending']}`} style={{ backgroundImage: !poster_path ? `url(${noImageAvailable})` : `${variables.gradient}, url(https://image.tmdb.org/t/p/w500${poster_path})` }}>
                             <BookmarkItem className={`${styles.mediacard__button}`} />
                             <div className={styles.mediacard__media_info}>
                                 <div className={`${styles.mediacard__media_date_and_type}  text_preset_5  text_white--opaque_75`}>
