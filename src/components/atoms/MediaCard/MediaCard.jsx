e556c5b8a63af5369ff3a9d6e4bb3d8c02c9dc1d
unknown <tommaso.duranti191@gmail.com>
2026-05-12 10:43:54 +0200

    Refactor MediaCard.jsx to exclude mediacard__infrom glass :hover effect

--- src/components/atoms/MediaCard/MediaCard.jsx
+++ src/components/atoms/MediaCard/MediaCard.jsx
@@ -14,20 +14,21 @@ import variables from '../../../sass/abstract/_export.module.scss';
 import getYearFormat from '../../../js/utils/date/date.js';
 
 export default function MediaCard({ isTrending, release_date, poster_path, media_type, video, id, avg_rating, title }) {
+    const isMovie = media_type === 'movie' || video !== undefined;
 
     return (
         <>
             {isTrending ?
                 (
                     <article className={`${styles.mediacard}  ${styles['mediacard--trending']}`}>
-                        <BookmarkItem className={`${styles.mediacard__button}`} />
-                        <Link className={styles.mediacard__link} to={`/${(media_type === 'movie' || video !== undefined) ? 'movie' : 'tv'}/${id}`} style={{ backgroundImage: !poster_path ? `url(${noImageAvailable})` : `${variables.gradient}, url(https://image.tmdb.org/t/p/w500${poster_path})` }}>
+                        <BookmarkItem className={styles.mediacard__button} />
+                        <Link className={styles.mediacard__link} to={`/${isMovie ? 'movie' : 'tv'}/${id}`} style={{ backgroundImage: !poster_path ? `url(${noImageAvailable})` : `${variables.gradient}, url(https://image.tmdb.org/t/p/w500${poster_path})` }}>
                             <div className={`${styles.mediacard__container}  ${styles['mediacard__container--trending']}`} >
                                 <div className={styles.mediacard__media_info}>
                                     <div className={`${styles.mediacard__media_date_and_type}  text_preset_5  text_white--opaque_75`}>
                                         <span className={`${styles.separator}  ${styles['separator--trending']}`}>{getYearFormat(release_date)}</span>
                                         <div className={`${styles.mediacard__media_category}  ${styles['separator']}  ${styles['separator--trending']}`}>
-                                            <img src={(media_type === 'movie' || video !== undefined) ? movie : tv} alt='' />
+                                            <img src={isMovie ? movie : tv} alt='' />
                                             <span className='text_capitalize'>{media_type}</span>
                                         </div>
                                         <span className='text_uppercase'>{avg_rating.toFixed(1)}</span>
@@ -38,24 +39,28 @@ export default function MediaCard({ isTrending, release_date, poster_path, media
                         </Link>
                     </article>
                 ) : (
-                    <article className={styles.mediacard}>
-                        <BookmarkItem className={`${styles.mediacard__button}`} />
-                        <Link className={styles.mediacard__link} to={`/${(media_type === 'movie' || video !== undefined) ? 'movie' : 'tv'}/${id}`} style={{ backgroundImage: !poster_path ? `url(${noImageAvailable})` : `url(https://image.tmdb.org/t/p/w500${poster_path})` }}>
-                            <div className={`${styles.mediacard__container}`} ></div>
-                        </Link>
-
-                        <div className={`${styles.mediacard__media_info}  ${styles.mediacard__text_outside}`}>
-                            <div className={`${styles.mediacard__media_date_and_type}  text_preset_6  text_white--opaque_75`}>
-                                <span className={styles.separator}>{release_date ? getYearFormat(release_date) : 'N/A'}</span>
-                                <div className={`${styles.mediacard__media_category}  ${styles['separator']}`}>
-                                    <img src={(media_type === 'movie' || video !== undefined) ? movie : tv} alt='' />
-                                    <span className='text_capitalize'>{(media_type === 'movie' || video !== undefined) ? 'movie' : 'tv'}</span>
-                                </div>
-                                <span className='text_uppercase'>{avg_rating.toFixed(1)}</span>
+                    <>
+                        <article >
+                            <div className={styles.mediacard}>
+                                <BookmarkItem className={styles.mediacard__button} />
+                                <Link className={styles.mediacard__link} to={`/${isMovie ? 'movie' : 'tv'}/${id}`} style={{ backgroundImage: !poster_path ? `url(${noImageAvailable})` : `url(https://image.tmdb.org/t/p/w500${poster_path})` }}>
+                                    <div className={styles.mediacard__container} ></div>
+                                </Link>
                             </div>
-                            <h3 className='text_preset_3  text_white  text_capitalize'>{title}</h3>
-                        </div>
-                    </article>
+
+                            <div className={`${styles.mediacard__media_info}  ${styles.mediacard__text_outside}`}>
+                                <div className={`${styles.mediacard__media_date_and_type}  text_preset_6  text_white--opaque_75`}>
+                                    <span className={styles.separator}>{release_date ? getYearFormat(release_date) : 'N/A'}</span>
+                                    <div className={`${styles.mediacard__media_category}  ${styles['separator']}`}>
+                                        <img src={isMovie ? movie : tv} alt='' />
+                                        <span className='text_capitalize'>{isMovie ? 'movie' : 'tv'}</span>
+                                    </div>
+                                    <span className='text_uppercase'>{avg_rating.toFixed(1)}</span>
+                                </div>
+                                <h3 className='text_preset_3  text_white  text_capitalize'>{title}</h3>
+                            </div>
+                        </article>
+                    </>
                 )
             }
         </>
