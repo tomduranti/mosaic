15a0b31d470685f1424faf7e59eae6165269da22
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-06-19 17:14:29 +0200

    Fix logo <a> to redirect to /mosaic/home (for prod)

--- src/components/atoms/NavBar/NavBar.jsx
+++ src/components/atoms/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ import avatar from "../../../assets/avatar/avatar.png";
 export default function NavBar() {
   return (
     <div className={styles.navbar}>
-      <a href="/home" className={styles.navbar__logo}>
+      <a href="/mosaic/home" className={styles.navbar__logo}>
         <img className={styles.navbar__logo} src={logo} alt="logo" />
       </a>
       <nav aria-label='navigation links'>
